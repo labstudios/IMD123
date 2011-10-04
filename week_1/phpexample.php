1bf3c444e4f84a7982d34ebc10fdc444196d1eb4
Brent <labstudios@gmail.com>
2011-10-04 01:47:16 +0000

    week 1 partly done

---
+++ week_1/phpexample.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>PHP Example</title>
+</head>
+<body>
+	<?php
+	echo "This is some stuff output by php";
+	?>
+</body>
+</html>
